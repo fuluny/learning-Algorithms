86a03c501c7d077ebf271f08fade0f8ec5dcb74d
fuluny <fulunyjang@gmail.com>
2017-07-04 17:05:26 -0700

    Update greedy.py

--- greedy.py
+++ greedy.py
@@ -1,17 +1,19 @@
-# greedy algorithm, change machine example
+# greedy algorithm, with change machine example.
+# Assuming array provided is sorted
+# either in descending or ascending order
+# A = [1,5,10,25] cent, nickel, dime, quarter for example
 
 # number to calculate and array to use to analyze
 def greedy(n,A):
-#   sort = O(nlogn)
-#    A.sort()
-#   reverse = O(n)
-#    A.reverse()
+    # check if calculation can be possible or is required
     if (n<0):
         print("Can't take negative values")
         return -1
     elif (n==0):
         print("No change required")
         return 0
+    # answer will be number of coins required for change
+    # given elements in array A, answer corresponds to A's order
     counter = [0]*len(A)
     if A.index(max(A)) > A.index(min(A)):
         for i in range(len(A)-1,-1,-1):
